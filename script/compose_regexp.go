ee90810633e823f76829d2821eba0e6be8c2b906
Maciej Pasternacki <maciej@3ofcoins.net>
2014-10-22 05:42:54 +0200

    Regexp/Compower :lipstick:

--- script/compose_regexp.go
+++ script/compose_regexp.go
@@ -2,38 +2,34 @@ package main
 
 import "fmt"
 
-import rx "github.com/3ofcoins/go-misc/regexp-composer"
+import . "github.com/3ofcoins/go-misc/regexp-composer"
 
 func main() {
-	b64 := rx.Some(rx.Base64Char) // Base64 stripped of `=` padding
 	fmt.Print(
-		rx.NewFile("cardea",
-			rx.MustVariable("TOKEN_RX",
-				rx.Beginning,
-				rx.AnyWhitespace,
-				rx.Capture("USERNAME", b64), // Capture username (may be base64)
-				rx.Capture("PAYLOAD",
-					rx.Alternation(
-						rx.Sequence(
-							// Legacy (Odin) payload
-							rx.Literal(","),
-							rx.Optional(rx.Capture("LEGACY_GROUPS", b64)),
-							rx.Literal(","),
-							rx.Capture("LEGACY_TIMESTAMP", rx.DecimalNumber),
-							rx.Literal(","),
-						),
-						rx.Sequence(
-							// Modern (Cardea URL-like) payload
-							rx.Literal(":"),
-							rx.Optional(
-								rx.Capture("FORMAT", rx.Word),
-								rx.Literal("?")),
-							rx.Capture("QUERY", `[^#]+`),
-							rx.Literal("#"),
-						),
-					)),
-				rx.Capture("HMAC", rx.HexNumber),
-				rx.AnyWhitespace,
-				rx.End,
-			)))
+		NewFile("cardea",
+			MustVariable("TOKEN_RX",
+				TrimAnchor(
+					Capture("USERNAME", Some(Base64Char)), // Username may be base64-encoded
+					Capture("PAYLOAD", // Payload (i.e. whatever's HMACed)
+						Alternation(
+							Sequence( // Legacy (Odin) payload
+								Literal(","),
+								Capture("LEGACY_GROUPS", Any(Base64Char)),
+								Literal(","),
+								Capture("LEGACY_TIMESTAMP", DecimalNumber),
+								Literal(","),
+							),
+							Sequence( // Modern (Cardea URL-like) payload
+								Literal(":"),
+								Optional(
+									// Optional "FORMAT?" prefix to indicate encryption
+									// or encoding of payload
+									Capture("FORMAT", Word),
+									Literal("?")),
+								Capture("QUERY", `[^#]+`), // URL query parameters
+								Literal("#"),
+							),
+						)),
+					Capture("HMAC", HexNumber),
+				))))
 }
